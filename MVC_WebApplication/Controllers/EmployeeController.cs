cc26e0280fb3e24dec905c80a622effdda599432
Nilesh Dhawade <dhawade.nilesh007@gmail.com>
2022-04-13 08:57:39 +0530

    Employee_Controller Added

---
+++ MVC_WebApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_WebApplication.Models;
+
+namespace MVC_WebApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult EmpList()
+        {
+            List<Employee> emp = new List<Employee>()
+            {
+                new Employee { empId = 1, empName= "Nilesh", empSalary=35000},
+                new Employee { empId = 2, empName= "Ganesh", empSalary=30000},
+                new Employee { empId = 3, empName= "Rajesh", empSalary=36000}
+
+            };
+            ViewBag.employee = emp;
+            return View();
+        }
+    }
+}
---
+++ MVC_WebApplication/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_WebApplication.Models
+{
+    public class Employee
+    {
+        public int empId { get; set; }
+        public string empName { get; set; }
+
+        public decimal empSalary { get; set; }
+    }
+}
